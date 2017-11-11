fa63965bc8141986129e3baa44a2b6447afae8ba
wylin <wylin.hit@gmail.com>
2017-11-11 22:25:02 +0800

    check if rdm for cmak

--- wylin-emacs/init-cmake-ide.el
+++ wylin-emacs/init-cmake-ide.el
@@ -10,18 +10,18 @@
 
 (require 'rtags) ;; optional, must have rtags installed
 (require 'company-rtags)
-(cmake-ide-setup)
+(if (executable-find "rdm") ;; check if rdm is installed
+    ((cmake-ide-setup)
+     (setq rtags-completions-enabled t)
+     (eval-after-load 'company
+       '(add-to-list
+         'company-backends 'company-rtags))
+     (setq rtags-autostart-diagnostics t)
+     (rtags-enable-standard-keybindings)
 
-(setq rtags-completions-enabled t)
-(eval-after-load 'company
-  '(add-to-list
-    'company-backends 'company-rtags))
-(setq rtags-autostart-diagnostics t)
-(rtags-enable-standard-keybindings)
+     (setq rtags-use-helm t)
 
-(setq rtags-use-helm t)
-
-(add-hook 'c-mode-common-hook 'rtags-start-process-unless-running)
-(add-hook 'c++-mode-common-hook 'rtags-start-process-unless-running)
-
+     (add-hook 'c-mode-common-hook 'rtags-start-process-unless-running)
+     (add-hook 'c++-mode-common-hook 'rtags-start-process-unless-running))
+  )
 (provide 'init-cmake-ide)
